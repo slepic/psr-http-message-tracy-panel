1819aa193507bee73c5e2800c0722370f9f02e1a
slepic <gslepic@gmail.com>
2019-05-04 10:16:47 +0200

    Update plain.php

--- examples/plain.php
+++ examples/plain.php
@@ -12,16 +12,16 @@ $storage = new \Slepic\Http\Transfer\Log\ArrayStorage();
 $panel = \Slepic\Tracy\Bar\PsrHttpMessagePanel\Factory::create($storage);
 
 //register the panel
-$tracy = \Tracy\Debugger::getBar();
-$tracy->addPanel($panel);
+$tracy = \Tracy\Debugger::getBar()->addPanel($panel);
+
+//now create history observer that will observe all http transfers and push logs to the storage
+$observer = new \Slepic\Http\Transfer\History\HistoryObserver($storage);
 
 //let's use a guzzle http client to generate some transfers
 $client = new \GuzzleHttp\Client();
 
-//now wrap the log storage in logging middleware for guzzle client
-$middleware = new \Slepic\Guzzle\Http\ObservingMiddleware\ObservingMiddleware(
-    new \Slepic\Http\Transfer\History\HistoryObserver($storage)
-);
+//we can use prepared observer adapter for guzzle client
+$middleware = new \Slepic\Guzzle\Http\ObservingMiddleware\ObservingMiddleware($observer);
 
 //register the middleware
 $client->getConfig('handler')
